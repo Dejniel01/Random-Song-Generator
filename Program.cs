a3ab99b8dacf73d8daee80fdce42c3067a2cafc6
dejniel01 <wujkowskidaniel01@gmail.com>
2022-05-19 00:21:11 +0200

    Add getting word functionality

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RestSharp;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Random_Song_Generator
 {
@@ -6,7 +9,31 @@ namespace Random_Song_Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var word = GetNewWord();
+            Console.WriteLine(word);
         }
+
+        static string GetNewWord()
+        {
+            var client = new RestClient("https://random-words-api.vercel.app");
+            var request = new RestRequest("/word");
+            var responseA = client.GetAsync(request);
+
+            var response = responseA.GetAwaiter().GetResult();
+
+            // using range indexes to trim array wrapper from Json file
+            var jsonString = response.Content[1..^1];
+
+            var data = JsonSerializer.Deserialize<WordClass>(jsonString,
+                       new JsonSerializerOptions() { WriteIndented = true });
+
+            return data.word;
+        }
+    }
+    class WordClass
+    {
+        public string word { get; set; }
+        public string definition { get; set; }
+        public string pronunciation { get; set; }
     }
 }
