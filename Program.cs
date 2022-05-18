e059d7593820d5da52e69c0a5214deffaa24dbd7
dejniel01 <wujkowskidaniel01@gmail.com>
2022-05-19 00:00:27 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Random_Song_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
